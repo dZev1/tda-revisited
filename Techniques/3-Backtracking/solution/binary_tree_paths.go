9839d89d8490e774d16e83b44ad4bd94bb035694
dZev1 <diegozevallos04@gmail.com>
2025-08-13 23:28:33 -0300

    globo gordo eh

--- Techniques/3-Backtracking/solution/binary_tree_paths.go
+++ Techniques/3-Backtracking/solution/binary_tree_paths.go
@@ -8,9 +8,24 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func (t *TreeNode) Equals(other *TreeNode) bool {
+	if t == nil && other == nil {
+		return true
+	}
+	if t == nil || other == nil {
+		return false
+	}
+	if t.Val != other.Val {
+		return false
+	}
+	return t.Left.Equals(other.Left) && t.Right.Equals(other.Right)
+}
+
 func BinaryTreePaths(root *TreeNode) []string {
 	paths := []string{}
-
+	if root == nil {
+		return paths
+	}
 	var backtrack func(node *TreeNode, path string)
 	backtrack = func(node *TreeNode, path string) {
 		leaf := node.Left == nil && node.Right == nil
--- Techniques/3-Backtracking/solution/solution_test.go
+++ Techniques/3-Backtracking/solution/solution_test.go
@@ -1,6 +1,9 @@
 package solution
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSubsetXORSum(t *testing.T) {
 	type args struct {
@@ -58,6 +61,44 @@ func TestCountMaxOrSubsets(t *testing.T) {
 			}
 		})
 	}
+}
 
-	
+func TestBinaryTreePaths(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct{
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "set of n elements",
+			args: args{root: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+					Right: &TreeNode{
+						Val: 5,
+					},
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},},
+			want: []string{"1->2->5", "1->3"},
+		},
+		{
+			name: "empty tree",
+			args: args{root: nil},
+			want: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BinaryTreePaths(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BinaryTreePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
